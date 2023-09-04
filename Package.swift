e2d08b1ca0f83ce00d17c08280b160b8ae74eb05
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-09-04 08:29:54 -0400

    Enable concurrency checking

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version:5.5
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let settings: [SwiftSetting] = [
-//	.unsafeFlags(["-Xfrontend", "-strict-concurrency=complete"])
+	.enableExperimentalFeature("StrictConcurrency")
 ]
 
 let package = Package(
-    name: "JSONRPC",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
-    products: [
-        .library(name: "JSONRPC", targets: ["JSONRPC"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(name: "JSONRPC", dependencies: [], swiftSettings: settings),
-        .testTarget(name: "JSONRPCTests", dependencies: ["JSONRPC"], swiftSettings: settings),
-    ]
+	name: "JSONRPC",
+	platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
+	products: [
+		.library(name: "JSONRPC", targets: ["JSONRPC"]),
+	],
+	dependencies: [
+	],
+	targets: [
+		.target(name: "JSONRPC", dependencies: [], swiftSettings: settings),
+		.testTarget(name: "JSONRPCTests", dependencies: ["JSONRPC"], swiftSettings: settings),
+	]
 )
