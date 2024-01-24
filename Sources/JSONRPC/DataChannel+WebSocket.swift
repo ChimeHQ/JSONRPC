7d4e1d402101ce41380e06a0bca6fa29eb2b5e6c
jbromberg <58891459+jbromberg@users.noreply.github.com>
2024-01-24 06:56:05 -0500

    Add WebSocket DataChannel (#12)
    * Add WebSocket DataChannel
    
    * Conditionalize linux, add readme entry
    
    ---------
    
    Co-authored-by: Matt <85322+mattmassicotte@users.noreply.github.com>

---
+++ Sources/JSONRPC/DataChannel+WebSocket.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+#if !os(Linux)
+extension DataChannel {
+    /// A channel that facilitates communication over WebSockets.
+    public static func webSocket(
+        url: URL,
+        terminationHandler: @escaping @Sendable () -> Void
+    ) throws -> DataChannel {
+        let socketSession: URLSession = .init(configuration: .default)
+        let socket: URLSessionWebSocketTask = socketSession.webSocketTask(with: url)
+        
+        socket.resume()
+        
+        let (stream, continuation) = DataSequence.makeStream()
+
+        Task {
+            while socket.state == .running {
+                do {
+                    let message = try await socket.receive()
+                    switch message {
+                    case .data(let data):
+                        continuation.yield(data)
+                    case .string(let string):
+                        continuation.yield(Data(string.utf8))
+                    @unknown default:
+                        fatalError("Unhandled message type")
+                    }
+                } catch {
+                    if socket.state == .canceling {
+                        terminationHandler()
+                    }
+                    continuation.finish()
+                    throw error
+                }
+            }
+        }
+        
+        let writeHandler: DataChannel.WriteHandler = {
+            try await socket.send(.data($0))
+        }
+        
+        return DataChannel(writeHandler: writeHandler, dataSequence: stream)
+    }
+}
+#endif
